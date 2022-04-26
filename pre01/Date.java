033dc11a7aec50cc45321cbb0e3ce7e278e93ba5
EmpressT 1Love <99760212+twardally@users.noreply.github.com>
2022-04-26 01:34:51 +0000

    Prework 2.12 Ex 2

---
+++ pre01/Date.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Date {
+    public static void main(String[] args){
+        String day = "Monday";
+          int date = 18;
+            String month = "April";
+              int year = 2022;
+      
+      
+      System.out.println(day);
+      System.out.println(date);
+      System.out.println(month);
+      System.out.println(year);
+
+      System.out.print(day + ", " + month + " " + date + ", " + year);
+    }
+}
